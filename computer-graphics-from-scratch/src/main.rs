f3ab8ea7c63e8e62ab096861bf064a14cf987722
Thomas Lisankie <id@tomlisankie.com>
2022-05-09 23:24:25 -0500

    Remove keydown event

--- computer-graphics-from-scratch/src/main.rs
+++ computer-graphics-from-scratch/src/main.rs
@@ -1,7 +1,6 @@
 extern crate sdl2;
 
 use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use std::time::Duration;
 
@@ -28,11 +27,7 @@ pub fn main() {
         canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
+                Event::Quit { .. } => break 'running,
                 _ => {}
             }
         }
